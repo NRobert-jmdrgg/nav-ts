4fc42e890342022635d23866395a44f149f526c0
Róbert Nagy <rob.iskola1@gmail.com>
2022-12-24 17:26:54 +0100

    array fix

--- src/nav/operations/types/error.ts
+++ src/nav/operations/types/error.ts
@@ -3,10 +3,10 @@ import { resSoftware, BasicHeader, BasicResult } from '../../baseTypes';
 import { TechnicalValidationMessagesType } from './response';
 
 export type GeneralExceptionResponse = {
-  funcCode: string; // feldolgozási eredmény
-  errorCode?: string; // a feldolgozási hibakód
-  message?: string; // Feldolgozási üzenet
-  notification?: string; // egyéb értesítés
+  funcCode: [string]; // feldolgozási eredmény
+  errorCode?: [string]; // a feldolgozási hibakód
+  message?: [string]; // Feldolgozási üzenet
+  notification?: [string]; // egyéb értesítés
 };
 
 export type GeneralErrorResponse = {
