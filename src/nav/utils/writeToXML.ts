aea777eca0b9651cec74d2e63e93d1c1b6a3101e
Róbert Nagy <rob.iskola1@gmail.com>
2022-12-25 00:47:28 +0100

    removenil

--- src/nav/utils/writeToXML.ts
+++ src/nav/utils/writeToXML.ts
@@ -1,15 +1,26 @@
+import { isNil, omitBy } from 'lodash';
 import { Builder } from 'xml2js';
 
 /**
  * Tetszőleges object kiírása xml-be
  * @param obj tetszőleges object
+ * @param rootname root neve
+ * @param removeNil undefined vagy null értékeket ne írja bele
  * @returns xml dokumentum string
  */
-export default function writeToXML(obj: any, rootname?: string): string {
+export default function writeToXML(
+  obj: any,
+  rootname?: string,
+  removeNil = true
+): string {
   let options;
 
   if (rootname) {
     options = { rootName: rootname };
+  }
+
+  if (removeNil) {
+    obj = omitBy(obj, isNil);
   }
 
   const xmlBuilder = new Builder(options);
