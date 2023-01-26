2158405600adfa461b84938a9cc6e8c4a25eee8d
Róbert Nagy <rob.iskola1@gmail.com>
2023-01-27 00:02:52 +0100

    uncomment

--- src/plugins/mongoose.ts
+++ src/plugins/mongoose.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance, FastifyPluginAsync } from 'fastify';
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 const connectDB: FastifyPluginAsync = async (fastify: FastifyInstance): Promise<void> => {
   try {
-    // await mongoose.connect(process.env.DB_CONNECT_URI as string);
-    // fastify.log.info('DB is Connecting...');
+    await mongoose.connect(process.env.DB_CONNECT_URI as string);
+    fastify.log.info('DB is Connecting...');
   } catch (error) {
     console.error(error);
   }
